fb23610d3761da780d84b2e461f7aedb3b56af96
Anastttasia <nastya.kaneva.99@gmail.com>
2025-11-27 18:31:09 +0300

    4 шаг. Напишем код, который будет соединять слой получения запроса и сохранения в БД.

--- src/main/java/ru/vladykinaar/data_service/controller/StudentController.java
+++ src/main/java/ru/vladykinaar/data_service/controller/StudentController.java
@@ -1,4 +1,37 @@
 package ru.vladykinaar.data_service.controller;
 
-public class StudentController {
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import ru.vladykinaar.data.api.StudentDataApi;
+import ru.vladykinaar.data.model.StudentDataCreateRequest;
+import ru.vladykinaar.data.model.StudentDataResponse;
+
+@RestController
+@RequiredArgsConstructor
+public class StudentController implements StudentDataApi {
+
+    private final StudentRepository studentRepository;
+
+    @Override
+    public ResponseEntity<StudentDataResponse> createStudentDataInData(StudentDataCreateRequest request) {
+        Student student = new Student();
+        student.setName(request.getFullName());
+        student.setPassport(request.getPassport());
+        studentRepository.save(student);
+        StudentDataResponse response = new StudentDataResponse();
+        response.setId(student.getId());
+        response.setFullName(student.getName());
+        response.setPassport(student.getPassport());
+
+        return ResponseEntity.status(200).body(response);
+    }
+
+  /*@Override
+  public ResponseEntity<StudentDataResponse> getStudentDataByIdFromData(Long id) {
+
+    return ResponseEntity.status(200).body(response);
+  }*/
 }
+
